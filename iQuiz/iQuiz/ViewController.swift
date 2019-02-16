23d6540b31eda0519e5acdbe975a9e61c8d62538
albertli354 <xl52@uw.edu>
2019-02-16 11:00:34 -0800

    complete part one

--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var settingButton: UIBarButtonItem!
+    var subjectsTitles = ["Mathematics", "Marvel Super Heroes", "Science"]
+    var shortDescription = ["The study of quantity, structure, space, and change", "Let's save the world", "A modern religion"]
+    var images = ["math.jpg", "marvel.png", "science.png"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
     }
-
-
+    
+    
+    @IBAction func settingsAction(_ sender: Any) {
+        let alert = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjectsTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "subject")!
+        cell.textLabel?.text = subjectsTitles[indexPath.row]
+        cell.detailTextLabel?.text = shortDescription[indexPath.row]
+        cell.imageView?.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    
+    
+    
+    
 }
+
+
 
