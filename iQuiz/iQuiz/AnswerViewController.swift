942fb11fddd5304696843def851f575242f80e36
albertli354 <xl52@uw.edu>
2019-02-16 20:08:24 -0800

    some ui

---
+++ iQuiz/iQuiz/AnswerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AnswerViewController.swift
+//  iQuiz
+//
+//  Created by Xiang Li on 2/16/19.
+//  Copyright © 2019 Xiang Li. All rights reserved.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
